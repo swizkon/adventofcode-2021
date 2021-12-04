1d76bb09b2e817f208a445420a79665322d81f80
Jonas Jerndin <jonas@queenslab.se>
2021-12-04 08:47:45 +0100

    Add some helpers and refactor

---
+++ DotNetHelpers/InputReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DotNetHelpers
+{
+    public class InputReader
+    {
+        public static string[] GetInput(string path, string fileName = "input.txt")
+        {
+            if (path == null)
+            {
+                return new List<string>().ToArray();
+            }
+
+            var filePath = Path.Combine(path, fileName);
+            if (File.Exists(filePath))
+            {
+                //Console.WriteLine("Found " + filePath);
+                return File.ReadAllLines(filePath);
+            }
+
+            var folder = new DirectoryInfo(path);
+            return GetInput(folder.Parent?.FullName, fileName);
+        }
+    }
+}
---
+++ day01-sonar-sweep/dotnet/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using DotNetHelpers;
+
+namespace SonarSweep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(typeof(Program).Namespace);
+
+            var input = InputReader.GetInput(Environment.CurrentDirectory);
+
+            var numberOfIncreases = FirstSolution(input);
+
+            Console.WriteLine(numberOfIncreases);
+        }
+
+
+        private static int FirstSolution(string[] input)
+        {
+            return input
+                    .Select(i => new
+                    {
+                        depth = Convert.ToInt32(i),
+                        increases = 0
+                    })
+                    .Aggregate(new
+                        {
+                            depth = int.MaxValue,
+                            increases = 0
+                        },
+                        (prev, next) =>
+                            new
+                            {
+                                next.depth,
+                                increases = next.depth > prev.depth ? prev.increases + 1 : prev.increases
+                            }, result => result.increases);
+        }
+    }
+}
