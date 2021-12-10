92ba5a3e7ad37c31090cf87cbdb20e18026fe988
Jonas Jerndin <jonas@queenslab.se>
2021-12-10 01:52:16 +0100

    Solution for day 6 A

--- python/day06/LanternFish.py
+++ python/day06/LanternFish.py
@@ -1,22 +1,33 @@
-from rx import of, operators as op
 
+# calc the spawn in a factorial way would be nice in erlang or elixir...
 
 def count_fishes(input):
-    vals = input.split(',')
+    vals = list(map(int, input.split(',')))
+
+    # Only need to calc for each unique
     unique_ages = list(set(vals))
-    print(input)
 
-    of(unique_ages).pipe(
-        op.distinct(lambda s: s),
-        #op.map(lambda s: len(s)),
-        #op.filter(lambda i: i >= 5)
-    ).subscribe(lambda value: print("Received {0}".format(value)))
+    result = 0
 
-    return 12345
+    for unique_age in unique_ages:
+        spawn = _calc_spawn([unique_age], 80)
+        # Multiply the result with the number of occurences in the input...
+        result += spawn * vals.count(unique_age)
+
+    return result
 
 
-def calculate_unique_ages(input):
+def _calc_spawn(fishes, days_left):
+    if days_left < 1:
+        return len(fishes)
 
-    print(input)
+    next_fishes = []
 
-    return 12345
+    for fish in fishes:
+        if fish == 0:
+            next_fishes.append(6)
+            next_fishes.append(8)
+        else:
+            next_fishes.append(fish - 1)
+
+    return _calc_spawn(next_fishes, days_left - 1)
--- python/main.py
+++ python/main.py
@@ -1,10 +1,12 @@
 import day01.SonarSweep as SonarSweep
+import day06.LanternFish as LanternFish
 
 
 def main():
     print("main")
 
-    _day01_a()
+    # _day01_a()
+    _day06_a()
 
 
 def _day01_a():
@@ -14,6 +16,18 @@ def _day01_a():
 
     print('SonarSweep.number_of_depth_increases')
     print(SonarSweep.number_of_depth_increases(input))
+
+
+def _day06_a():
+    input = _read_file_as_string('day06')
+
+    print('LanternFish.count_fishes')
+    print(LanternFish.count_fishes(input))
+
+
+def _read_file_as_string(day):
+    file1 = open('../input-' + day + '.txt', 'r')
+    return file1.read()
 
 
 def _read_file(day):
